af31144a93010444411ab3256e5a516c1db6c3f5
Cydaaa <81217053+Cydaaa@users.noreply.github.com>
2021-08-03 03:03:00 +0800

    Update CREDS.bat

--- CREDS.bat
+++ CREDS.bat
@@ -1,7 +1,5 @@
 @echo off
-echo All done! Connect your VM using RDP.
-echo IP:
-tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't get NGROK tunnel, be sure NGROK_AUTH_TOKEN is correct in Settings> Secrets> Repository secret. Maybe your previous VM still running: https://dashboard.ngrok.com/status/tunnels "
+echo All done! Connect to your VM using RDP.
 echo User: Administrator
 echo Pass: Fubuki@1210
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& './DisablePasswordComplexity.ps1'" > out.txt 2>&1
