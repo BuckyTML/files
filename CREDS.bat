8e155fcf683f194b6534977bf0c671cd4f6bd4a9
Cydaaa <81217053+Cydaaa@users.noreply.github.com>
2021-08-03 03:09:17 +0800

    Update CREDS.bat

--- CREDS.bat
+++ CREDS.bat
@@ -1,5 +1,6 @@
 @echo off
 echo All done! Connect to your VM using RDP.
+echo IP: You'll find it here: https://dashboard.ngrok.com/endpoints/status
 echo User: Administrator
 echo Pass: Fubuki@1210
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& './DisablePasswordComplexity.ps1'" > out.txt 2>&1
